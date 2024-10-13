607b500eb7ad9b3f1f6ff5f9040f3bd97ab33769
hilalnur1010@gmail.com <hilalnur1010@gmail.com>
2024-10-13 12:38:57 +0300

    Ders 5

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region For Döngüsü
+
+            //int i;
+            //for(i=1; i<=5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for(int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i < 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen Başlangıç değeri giriniz : ");
+            //int başlangıc = int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen Bitiş değeri giriniz : ");
+            //int bitis=int.Parse(Console.ReadLine());
+
+            //for(int i = başlangıc; i <= bitis; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            #region For Döngüsü ve Karar Yapıları
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalValue = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    totalValue += i;
+            //}
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0;
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine("-----------");
+            //Console.WriteLine(totalValue);
+
+            //int sayı = 0;
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        sayı++;
+            //    }
+            //}
+            //Console.Write(sayı);
+
+            #endregion
+
+            #region Örnek
+
+            //1-2-4-8-16
+            //int bacterium = 1;
+
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i+". saat sonunda : "+bacterium);
+            //}
+
+            #endregion
+
+            #region While Döngüsü
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0) {
+
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Örnek2
+
+            //Console.Write("Sayı Giriniz : ");
+            //int number=int.Parse(Console.ReadLine());
+            //int ones, tens, hundred;
+            //int sum = 0;
+            ////487
+            //ones = number % 10;
+            //tens = (number % 100)/10;//          87 / 10: 8
+            //hundred = number / 100; 
+
+            //sum = ones + tens + hundred;
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Alt Alta 10 tane yıldız oluşturma
+
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan Yana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt Alta 10 tane yıldız oluşturma her satırda 10 yıldız olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+            #region Dik Üçgen oluşturma
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i  ; j++) {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Üçgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Dik Üçgen
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 4; i >= 1; i--)
+            //{
+            //    for(int j=1; j <=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava dilimi
+
+            //int yukseklik = 5; // kaç satırdan oluşacağını belirledi
+            ////üst kısım
+            //for(int i = 1; i <= yukseklik; i++)
+            //{
+            //    for(int j = yukseklik ; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1;k<=2*i-1; k++) //2*i-1      1 - 3 - 5 - 7 yıldız sayısını sağlıyor
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //// alt kısım
+            //for (int i = yukseklik - 1; i >= 1; i--)
+            //{
+            //    for (int j = yukseklik ; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            #endregion
+
+            #region Piramit
+
+            //int yukseklik = 5;
+
+            //for (int i = 1; i <= yukseklik; i++) 
+            //{
+            //    // boşluklar
+            //    for (int j = yukseklik - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    // yıldızlar
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Piramit
+
+            int yukseklik = 5;
+            for (int i = yukseklik; i >=1; i--)
+            {
+                // boşluklar
+                for(int j = yukseklik-i; j>0; j--)
+                {
+                    Console.Write(" ");
+                }
+                // yıldızlar
+                for(int k = 1; k <= 2*i-1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
